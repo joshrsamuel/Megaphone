ae6f036df64a51155908cac759b829842cbf4112
Hai Dao <haixdao0923@gmail.com>
2022-02-03 15:05:52 -0500

    delete default react landing page

--- megaphone/src/App.js
+++ megaphone/src/App.js
@@ -1,23 +1,12 @@
+import Textbox from './Components/Textbox';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hello!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App App-header">
+      <Textbox name="Hai" imageSource="https://cdn5.vectorstock.com/i/1000x1000/24/14/mountain-vector-21382414.jpg"/>
+      <Textbox name="Test"></Textbox>
     </div>
   );
 }
